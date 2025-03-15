4a516d48b51a75477d6a06f15a25188682cef095
Jefri Maulana <60543355+Jeff-04@users.noreply.github.com>
2025-03-15 17:35:12 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -331,7 +331,7 @@ st.markdown("""
 st.write("### My Projects")
 
 # Membuat 3 kolom dengan layout st.columns
-col1, col2, col3 = st.columns(3)
+col1, col2, col3 = st.columns(3, gap='large')
 
 # Kotak pertama di kolom pertama
 with col1:
