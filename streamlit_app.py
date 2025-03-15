0f2bda53f79f4f4da43fd57fbe60b1cbb96ab453
Jefri Maulana <60543355+Jeff-04@users.noreply.github.com>
2025-03-15 17:38:09 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -349,33 +349,29 @@ with col1:
 
 # Kotak kedua di kolom kedua
 with col2:
-    st.markdown("""
-        <div style="height: 150px; padding: 20px; border: 2px solid #fff; border-radius: 10px; background-color: #63E0F7; text-align: center;">
-            <h3 style="color: white; font-weight: bold; margin-bottom: 10px;">
-                Scholarship Selection System using SVM & SMART
-            </h3>
-            <p style="font-size: 12px; color: white; margin-bottom: 10px;">
+     st.markdown("""
+        <div style="width: 250px; height: 300px; padding: 20px; border: 2px solid #fff; border-radius: 10px; display: inline-block; background-color: #24242F;">
+            <h6 style="color: white; font-weight: bold; margin-bottom: 10px; text-align: center;">
+                Medical Detection (MediDetect)
+            </h6>
+            <p style="font-size: 14px; color: white; margin-bottom: 10px; text-align: justify;">
                 Sistem deteksi penyakit berdasarkan gejala menggunakan Machine Learning yang diintegrasikan dengan API (FastAPI).
             </p>
-            <div style="display: flex; justify-content: center; align-items: center; flex-grow: 1;">
-                <a href="https://www.example.com" style="color: white; text-decoration: none; padding: 10px 20px; background-color: #4CAF50; border-radius: 5px; font-size: 14px;">explore</a>
-            </div>
+            <a href="https://github.com/Jeff-04/MediDetect" style="color: white; justify-content: center; text-decoration: none; padding: 5px 10px; background-color: #4CAF50; border-radius: 5px; font-size: 14px;">explore</a>
         </div>
     """, unsafe_allow_html=True)
 
 # Kotak ketiga di kolom ketiga
 with col3:
-    st.markdown("""
-        <div style="height: 150px; padding: 20px; border: 2px solid #fff; border-radius: 10px; background-color: #63E0F7; text-align: center;">
-            <h3 style="color: white; font-weight: bold; margin-bottom: 10px;">
-                Scholarship Selection System using SVM & SMART
-            </h3>
-            <p style="font-size: 12px; color: white; margin-bottom: 10px;">
+     st.markdown("""
+        <div style="width: 250px; height: 300px; padding: 20px; border: 2px solid #fff; border-radius: 10px; display: inline-block; background-color: #24242F;">
+            <h6 style="color: white; font-weight: bold; margin-bottom: 10px; text-align: center;">
+                Medical Detection (MediDetect)
+            </h6>
+            <p style="font-size: 14px; color: white; margin-bottom: 10px; text-align: justify;">
                 Sistem deteksi penyakit berdasarkan gejala menggunakan Machine Learning yang diintegrasikan dengan API (FastAPI).
             </p>
-            <div style="display: flex; justify-content: center; align-items: center; flex-grow: 1;">
-                <a href="https://www.example.com" style="color: white; text-decoration: none; padding: 10px 20px; background-color: #4CAF50; border-radius: 5px; font-size: 14px;">explore</a>
-            </div>
+            <a href="https://github.com/Jeff-04/MediDetect" style="color: white; justify-content: center; text-decoration: none; padding: 5px 10px; background-color: #4CAF50; border-radius: 5px; font-size: 14px;">explore</a>
         </div>
     """, unsafe_allow_html=True)
 
